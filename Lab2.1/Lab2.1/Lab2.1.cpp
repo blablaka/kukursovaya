fcbd51f8a1a306158671f30a6dc4fe36f065fe74
RubickAnT <43210239+RubickAnT@users.noreply.github.com>
2019-03-27 15:33:22 +0200

    chto-to
    chot-to

---
+++ Lab2.1/Lab2.1/Lab2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[8], count = 0;
+	cout << "Enter mas: \n";
+
+	for (int i = 0; i < 8; i++)
+		cin >> a[i];
+
+	for (int i = 0; i < 8; i++)
+		if (a[i] > 0) { count++; }
+
+	cout << "Kol-vo = " << count;
+
+	cin.get();
+	cin.get();
+}
