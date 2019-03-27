dfba9e076a1a815aeccc5a7b6d3bed197349ce64
keepo26062 <keepo26062@mail.ru>
2019-03-27 16:10:12 +0200

    laba2
    laba2 vitalik

---
+++ Vitalik/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "pch.h"
+#include <iostream>
+#include <stack>
+#include <locale.h>
+
+using namespace std;
+
+int main(void) {
+
+	setlocale(LC_ALL, "rus");
+	stack<int> stack1;
+	stack<int> stack2;
+	for (int i = 0; i < 5; i++) {
+		cout << i+1 << " ";
+	}
+	cout << " - первый до обмена\n";
+	for (int i = 0; i < 5; i++) {
+		cout << i + 20 << " ";
+	}
+	cout << " - второй до обмена\n";
+	for (int i = 0; i < 5; i++) {
+		stack1.push(i + 1);
+	}
+	for (int i = 0; i < 5; i++)
+		stack2.push(i + 20);
+	swap(stack1, stack2);
+	cout << "первый после обмена: ";
+	while (!stack1.empty()) {
+		cout << stack1.top() << " ";
+		stack1.pop();
+	}
+	cout << endl;
+	cout << "\nвторой после обмена: ";
+	while (!stack2.empty()) {
+		cout << stack2.top() << " ";
+		stack2.pop();
+	}
+
+	cout << endl;
+
+	return 0;
+}
---
+++ Vitalik/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <conio.h>
+
+void main()
+{
+	const int size = 11;
+	int mas[size] = { 11,13,15,26,27,317,322,334,348,540,154686486 };
+	int min = mas[0];
+	int index = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (mas[i] < min)
+		{
+			min = mas[i];
+			index = i;
+		}
+	}
+	std::cout << min << std::endl;
+}
